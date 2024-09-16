90d666bb6c26826048b720e740e1f12d57e66eec
n-asfaw <nardosasfaw77@gmail.com>
2024-09-16 13:53:55 +0300

    optional task issue solved

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,18 +1,15 @@
 def main():
+    monthly_income = float(input("Enter your monthly income: "))
+    total_expenses = float(input("Enter your total monthly expenses: "))
     
-        monthly_income = float(input("Enter your monthly income: "))
-        total_expenses = float(input("Enter your total monthly expenses: "))
-        
-        monthly_savings = monthly_income - total_expenses
-        
-        annual_interest_rate = 0.05
-        annual_savings = monthly_savings * 12
-        projected_savings = annual_savings + (annual_savings * annual_interest_rate)
-        
-        print(f"Your monthly savings are ${monthly_savings:.2f}.")
-        print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
+    monthly_savings = monthly_income - total_expenses
     
+    annual_interest_rate = 0.05
+    annual_savings = monthly_savings * 12
+    projected_savings = annual_savings + (annual_savings * annual_interest_rate)
     
-
+    print(f"Your monthly savings are ${monthly_savings:.2f}.")
+    print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
 
 main()
+
