41ac9ddb0642d851129f142197738e49072ab422
n-asfaw <nardosasfaw77@gmail.com>
2024-09-25 10:03:08 +0300

    trial 4

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
-def perform_operation(num1 ,num2 ,operation):
-
+def perform_operation(num1, num2, operation):
     if operation == 'add':
-      return num1 + num2
-    elif operation =='subtract':
-      return num1 - num2
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
     elif operation == 'multiply':
-       return num1 * num2
+        return num1 * num2
     elif operation == 'divide':
-       if num2 == 0:
-         return "Division by 0 is not allowed"
-       else:
-         return num1 / num2 
-    else :
-        return "Invalid operation please use add,subtract,multiply or divide"     
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        return num1 / num2
+    else:
+        return "Error: Invalid operation. Please choose from add, subtract, multiply, or divide."
+  
