4d9d9b270068779fc18259f7f398415fa3f7d8a3
n-asfaw <nardosasfaw77@gmail.com>
2024-09-25 16:40:55 +0300

    task 2 error fixed

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -2,10 +2,10 @@
 
 def display_menu():
     print("\n--- Shopping List Manager ---")
-    print("1. Add Item")
-    print("2. Remove Item")
-    print("3. View List")
-    print("4. Exit")
+    print(f"1. Add Item")
+    print(f"2. Remove Item")
+    print(f"3. View List")
+    print(f"4. Exit")
     print("-----------------------------")
 
 def main():
